b0f2136db9a617049339a0b232f98c4011a868d6
ChakirKharraz <125362489+ChakirKharraz@users.noreply.github.com>
2023-11-22 12:08:06 +0100

    CSS + Second player + LIMITE LA TAILLE MIN ET MAX grid et win

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,15 @@ class Game(models.Model):
     def get_absolute_url(self):
         return reverse('play', kwargs={'pk': self.id})
 
+    def save(self, *args, **kwargs):
+        # Ensure grid_size is between 3 and 12
+        self.grid_size = max(3, min(self.grid_size, 12))
+
+        # Ensure win_size is between 3 and grid_size
+        self.win_size = max(3, min(self.win_size, self.grid_size))
+
+        # Check if game_player2 is not set, and a different user is joining
+        if not self.game_player2 and self.game_author != self.game_player2:
+            self.game_player2 = self.game_player2
+
+        super().save(*args, **kwargs)
