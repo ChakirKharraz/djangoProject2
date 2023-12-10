0f8b931df36c6a4f3c8e73d1558779f31a694b29
ChakirKharraz <125362489+ChakirKharraz@users.noreply.github.com>
2023-12-11 00:07:19 +0100

    Generate Smbol au hasard

--- users/models.py
+++ users/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import random
+import string
+
+
+def generate_default_symbol():
+    letter = random.choice(string.ascii_uppercase)
+    number = random.randint(0, 9)
+    return f'{letter}{number}'
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
-    symbol = models.CharField(max_length=1, default='X')
+    symbol = models.CharField(max_length=2, default=generate_default_symbol)
     score = models.IntegerField(default=0)
-    game_played =  models.IntegerField(default=0)
+    game_played = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -21,4 +29,3 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
